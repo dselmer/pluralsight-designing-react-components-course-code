7e778ba283e00e4e8184e896be475039a93e1c3d
dselmer <dselmer@icstars.org>
2024-04-12 10:51:37 -0500

    first commit

--- 02-designing-better-components/clip-05-building-functional-components/pages/index.js
+++ 02-designing-better-components/clip-05-building-functional-components/pages/index.js
@@ -1,5 +1,20 @@
+import {data} from '../SpeakerData';
+
 const IndexPage = () => {
-  return <div>Hello From Pluralsight!</div>;
+  return( 
+  <div className='container speakers-list'>
+    <div className='row'>
+      <div className='col-xs-12 col-sm-12 col-md-6 col-lg-14 col-sm-12 col-xl-12'>
+        <div className='card card-height p-4 mt-4'>
+          <div className='speaker-img d-flex flex-row justify-content-center align-items-center h-300' >
+            <img className='content-fit'/>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  
+)
 };
 
 export default IndexPage;
